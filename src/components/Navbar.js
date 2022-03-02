95fca19a22f2af2173b22ce5ff401cc1a1460bb3
AdityaPandey03 <pandey.aditya4272@gmail.com>
2022-03-02 21:45:42 +0530

    some changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/AdityaPandey03"
+                href="https://github.com/AdityaPandey03/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
