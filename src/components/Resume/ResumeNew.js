5ca40ed284f7f84a629cf8746ee09419cbcecab3
AdityaPandey03 <pandey.aditya4272@gmail.com>
2022-03-02 22:22:41 +0530

    build changes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,8 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://github.com/AdityaPandey03/MyDashboard/blob/32202f903e5cfcf3870b675947f3592c8fad70ea/Aditya_Pandey_Resume_.pdf";
+
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
